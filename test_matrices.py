5d39f6e9f3df0387c4a36ad7c86091d2fc90d32f
pramodmsm <pramodsmurthy25@gmail.com>
2022-02-06 12:35:43 -0500

    added a new function to get_data()

--- test_matrices.py
+++ test_matrices.py
@@ -1,26 +1,34 @@
+from pkgutil import get_data
 from types import resolve_bases
 import unittest
+import numpy as np
 import reeb
 
-
 class testing(unittest.TestCase):
-    
-    def get_data(size):
-        matrix2 =   [
+     
+      
+    def get_data(self,random=False,size=(3,3)):
+        if not random and size==(3,3):
+            matrix1 = np.array(  [   
+                    [1, 6, 3],
+                    [2, 7, 4],
+                    [5, 8, 9]
+                    ])
+            return matrix1
+        elif not random and size==(4,4):
+            matrix2 = np.array(  [
                     [1,7,12,13],
                     [9,2,6,5 ],
                     [14,8,11,10],
                     [15,16,4,3]
-                    ]
-        return np.random.randint(1,16,size=(4,4))      
-    
+                    ])
+            return matrix2
+        else:
+            return np.random.randint(1,16,size=(4,4))   
+
     def test_graph(self):
-        matrix1 = [  [1, 6, 3],
-                                    [2, 7, 4],
-                                    [5, 8, 9]
-                                ]
         reference_parent = [-1, 2, 5, 4, 6, 7, 7, 8, 9]
-        self.assertEqual(reeb.get_reeb(matrix1),reference_parent)
+        self.assertEqual(reeb.get_reeb(self.get_data(size=(3,3))),reference_parent)
        
 
 
