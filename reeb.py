c3e53c159023620e4f56e3e4a2670409bc3825d7
Pramodmsm <pramodsmurthy25@gmail.com>
2022-09-08 23:41:14 -0400

    Final submission with documented code

--- reeb.py
+++ reeb.py
@@ -1,38 +1,71 @@
-from prompt_toolkit import prompt
-from src.Undirected import Undirectedgraph,Utils
-
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import pprint
 
-              
+from prompt_toolkit import prompt
+from src.Undirected import Undirectedgraph,Utils
+from copy import deepcopy
+
+# function to build the reeb given the 2D scalar field/matrix              
 def get_reeb(m,tree_type):
     g = Undirectedgraph(m,tree_type=tree_type)
     G = nx.Graph()
     processed_nodes = []
-                                                                                 # O(n)  space complexity
+    
+    # iterating over every node to build tree                                                             
     for node in g.m_flat:
         processed_nodes = g.build_tree(node,processed_nodes)
     
-    return processed_nodes,g
+    processed_nodes = processed_nodes.pop()
+    initial_g = deepcopy(g.graph)
+    
+    Utils.print(f'augmenting tree\n')
+    g.augment(root=np.random.choice(processed_nodes,1)[0])
+   
+    return processed_nodes,initial_g,g.graph
+    
     
 if __name__ == "__main__":
-    Utils.verbose = True
-    unit_size = int(input('enter unit size as integer :'))
-    # tree_type = str(input("enter type of tree 'merge/split' :"))
+    unit_size = int(input('enter unit size as (any integer > 0) :'))
+    tree_type = str(input("enter type of tree 'merge/split' :"))
+    debug_mode = str(input("enter debug mode 'yes/no' :"))
+    Utils.verbose = True if debug_mode=='yes' else False
     scalar_field = np.random.choice(np.arange(1,np.square(unit_size)+3),size=(unit_size,unit_size),replace=False)
     Utils.print(f'input scalar field is\n\n {scalar_field}')
     
-    _,merge_tree = get_reeb(scalar_field,tree_type='merge')
-    _,split_tree = get_reeb(scalar_field,tree_type='split')
+    if tree_type=='merge':
+        _,merge_tree,aug_merge_tree = get_reeb(scalar_field,tree_type='merge')
+        pprint.pprint('merge tree')
+        pprint.pprint(merge_tree,compact=False,width=40)
+        pprint.pprint('final reeb graph')
+        pprint.pprint(aug_merge_tree,compact=False,width=40)
+        Gm = nx.Graph(merge_tree)
+        Gm_a = nx.Graph(aug_merge_tree)
+    else:
+        _,split_tree,aug_split_tree = get_reeb(scalar_field,tree_type='split')
+        pprint.pprint('split tree')
+        pprint.pprint(split_tree,compact=False,width=40)
+        pprint.pprint('final reeb graph')
+        pprint.pprint(aug_split_tree,compact=False,width=40)
+        Gs = nx.Graph(split_tree)
+        Gs_a = nx.Graph(aug_split_tree)
 
+    # plots     
+    plt.figure(figsize=(40,40))
+    pprint.pprint('plotting visualizations')
+    if tree_type=='merge':
+
+        plt.subplot(121)
+        nx.draw_networkx(Gm, font_weight='bold')
+        plt.subplot(122)
+        nx.draw_networkx(Gm_a, font_weight='bold')
     
-    Gm = nx.Graph(merge_tree.graph)
-    Gs = nx.Graph(split_tree.graph)
-    Utils.print([merge_tree.graph])
-    plt.figure(figsize=(20,20))
-    plt.subplot(121)
-    nx.draw_networkx(Gm, font_weight='bold')
-    plt.subplot(122)
-    nx.draw_networkx(Gs, font_weight='bold')
+    else:
+        plt.subplot(121)
+        nx.draw_networkx(Gs, font_weight='bold')
+        plt.subplot(122)
+        nx.draw_networkx(Gs_a, font_weight='bold')
+    
     plt.show()
+
--- src/Undirected.py
+++ src/Undirected.py
@@ -1,8 +1,10 @@
-from collections import defaultdict
+# class for building a tree given a scalar field to obtain an augmented join tree
+
 from tabnanny import verbose
-import numpy as np
 from pipe import where,select,traverse,dedup
-#This class represents a undirected graph using adjacency processed_nodes representation
+from collections import defaultdict
+from copy import deepcopy
+from src.utils import Stack
 
 class Utils(object):
 	verbose: bool = True
@@ -19,11 +21,27 @@ class Utils(object):
 			# del kwargs['verbose']
 			print(*args, **kwargs)
 
-class Undirectedgraph:
 
+
+class Undirectedgraph:
+	'''
+		Purpose
+			This class constructs an undirected graph from a given 2D scalar field
+		
+		Args
+			m			:	input 2D scalar field/integer matrix
+			tree_type 	:	merge/split (optional)
+							if tree_type == 'merge' --> nodes are processed in ascending 
+							order
+							if tree_type == 'split' --> nodes are processed in descending
+							order
+	'''		
+	
 	def __init__(self,m,tree_type='merge'):
 		self.graph = defaultdict(list) # default dictionary to store graph
 		self.m = m
+		self.min = m.min()
+		self.max = m.max()
 		self.tree_type = tree_type
 		self.m_flat = self.m.flatten()
 		self.m_flat.sort()
@@ -31,54 +49,49 @@ class Undirectedgraph:
 			self.m_flat = np.flip(self.m_flat)
 		Utils.print(self.m_flat)
 	
+	# reprenstation of a graph
 	def __repr__(self) -> dict:
 		return self.graph
+	
+	# degree of a node 
+	def degree(self,node) -> int:
+		return len(self.graph[node])
 	
 	# function to add an edge to graph
 	def addEdge(self,u,v):
 		self.graph[u].append(v)
 		self.graph[v].append(u)
-
-	# function to check an edge in graph given nodes (u,v)
-	def isEdge(self,u,v):
-		if u<v and v in self.graph[u] :
-			return True
-		return False
-
+		
+	# method to delete a node and its connected edges
+	def delete(self,n):
+		list(self.graph[n] | select(lambda x:self.graph[x].remove(n)))
+		del(self.graph[n])
+	
 	# get adjacent neighbours of a node in tree 
 	def get_neighbours(self,node):
 		return self.graph[node]
 	
-	# A utility function to find the root parent of an element i
-	def find_parent(self, parent,i):
-		if parent[i] == -1:
-			return i
-		if parent[i]!= -1:
-			return self.find_parent(parent,parent[i])
-
-	# A utility function to do union of two subsets
-	def union(self,parent,x,y):
-		parent[x] = y
-
-
+	# utility to get indices of element in the 2D scalar field
 	def get_index(self, item):
 		[x, y] = np.where(self.m == item)
 		x, y = x[0], y[0]
 		return x, y
 	
-	def is_neighbour(self,children,node):
+	# utility to check if any of the elements are neighbours to node
+	def is_neighbour(self,elements,node):
 		[r, c] = self.m.shape
 		[x, y] = self.get_index(node)
 
-		for child in children:
-			if child in self.m[max(0,x-1):min(x+2,r), max(y-1,0):min(y+2, c)]:
+		for element in elements:
+			if element in self.m[max(0,x-1):min(x+2,r), max(y-1,0):min(y+2, c)]:
 				return True
 		
 		return False
     
+	# utility to merge connected components
 	def merge_sets(self,sets,node):
 		con_com = {node:[],-1:[]}
-		# Utils.print(f'before merging processed_nodes{sets}')
+		Utils.print(f'before merging processed_nodes: {sets}')
 		for subset in sets:
 			if node in subset:
 				con_com[node].append(subset)
@@ -90,21 +103,56 @@ class Undirectedgraph:
 		if len(con_com[-1])!= 0:
 			for i in con_com[-1]: sets.append(i)
 		con_com.clear()
-		Utils.print(f'post merging processed_nodes{sets}')
+		Utils.print(f'post merging processed_nodes: {sets}')
 		return sets
-
+	
+	# main function to build either trees passing single node to given tree
 	def build_tree(self,node,processed_nodes):
+		'''
+			Args
+				node			:	a new node to be added to build the tree
+				processed_nodes	:	set of nodes already part of the tree/processed 
+			
+			Purpose
+				build the tree by adding every node based on neighbourhood in scalar field
+				and connecting components/nodes
+			
+			Example
+				input scalar field is
+							   [[ 1 21  8 25 27]
+								[ 7 16 24 23  5]
+								[ 2  4  9 18 11]
+								[13 20 26 10 22]
+								[ 6 17 12 15 14]]
+				processed_nodes=[]
+				
+				subset [1] from processed nodes [[1]], node is 2
+				is node 2 a neighbour of subset[1] : False
+
+				subset [1] from processed nodes [[1], [2]], node is 4
+				is node 4 a neighbour of subset[1]: False
+
+				subset [2] from processed nodes [[1], [2]], node is 4
+				is node 4 a neighbour of subset[2]: True
+				appending node 4 to processed_nodes [[1], [2]]
+				before merging processed_nodes[[1], [2, 4]]
+				post merging processed_nodes[[2, 4], [1]]
+				...
+		'''
 		flag_no_neigbhour = True
 		if len(processed_nodes)!=0:
 			for idx,subset in enumerate(processed_nodes):
+				
 				Utils.print(f'\nsubset {subset} from processed nodes {processed_nodes}, node is {node}')
-				Utils.print(f'is node {node} a neighbour of subset{subset}{self.is_neighbour(subset,node)}')
+				Utils.print(f'is node {node} a neighbour of subset {subset}: {self.is_neighbour(subset,node)}')
+				
 				if self.is_neighbour(subset,node):
 					if self.tree_type == 'merge':
 						self.addEdge(np.max(subset),node)
 					else:
 						self.addEdge(np.min(subset),node)
-					Utils.print(f'appending node {node} to processed_nodes {processed_nodes}')
+					Utils.print(f'merging node {node} to subset {subset} in processed_nodes')
+				
 					processed_nodes[idx].append(node)
 					flag_no_neigbhour = False
 
@@ -115,3 +163,36 @@ class Undirectedgraph:
 		else:
 			processed_nodes.append([node])
 		return processed_nodes
+		
+	
+	# optimized iterative algorithm 
+	def augment(self,root: int):
+		'''
+		Args
+			root:any node in graph 
+
+		Purpose
+			Iterative method of Graph augmentation of graph retaining only critical nodes(a.k.a saddle points)
+			and root nodes reducing the size of graph considerably
+
+		Output
+			returns: None
+
+		time complexity: O(n^2)
+		Space complexity: O(n)
+		'''
+		to_visit=[]
+		to_visit.append(root)
+		counter=0
+		
+		while(counter<len(to_visit)):
+			node=to_visit[counter]
+			neighbours=self.get_neighbours(node)
+			if self.degree(node)==2:
+				self.addEdge(neighbours[0],neighbours[1])
+				self.delete(node)
+			to_visit=to_visit+list(neighbours | where(lambda x: x not in to_visit))
+			counter+=1
+
+		del(to_visit)
+		del(counter)
